d4124c460393deb2460b0a7372898deddcb5cb48
Aditya1409 <parekhaditya14@gmail.com>
2023-12-13 18:29:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recipeapp/Listeners/RecipeDetailsListener.java
@@ -0,0 +1,9 @@
+package com.example.recipeapp.Listeners;
+
+import com.example.recipeapp.Models.RecipeDetailsResponse;
+
+public interface RecipeDetailsListener {
+    void didFetch(RecipeDetailsResponse response, String message);
+    void didError(String message);
+
+}
